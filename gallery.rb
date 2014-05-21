86d6a9ae3463302766f29a693c4749079b51d195
Alban Peignier <alban@tryphon.eu>
2014-05-21 20:19:56 +0200

    Defines Hash#deep_merge if missing

--- gallery.rb
+++ gallery.rb
@@ -21,6 +21,20 @@ class Dir
     end
 end
 
+class Hash
+  def deep_merge(other_hash)
+    dup.deep_merge!(other_hash)
+  end
+
+  def deep_merge!(other_hash)
+    other_hash.each_pair do |k,v|
+      tv = self[k]
+      self[k] = tv.is_a?(Hash) && v.is_a?(Hash) ? tv.deep_merge(v) : v
+    end
+    self
+  end
+end unless Hash.respond_to?(:deep_merge)
+
 # odie gallery plugin
 # ------------------------------------------------------------------------------
 module Jekyll
